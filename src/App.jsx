49ba0311b5587ad398acbd582d3c3f5c977fc795
shaifali <shaifalikatiyar428@gmail.com>
2024-12-31 20:19:57 +0530

    TODO_List created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { ToDo } from "./Project/ToDo";
+
+//import "./ToDo.jsx";
+function App() {
+	return (
+		<>
+			<ToDo></ToDo>
+		</>
+	);
+}
+
+export default App;
---
+++ src/Project/ToDo.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { MdCheck } from "react-icons/md";
+import { MdDeleteForever } from "react-icons/md";
+export const ToDo = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [task, setTask] = useState([]);
+	const handleChange = (value) => {
+		setInputValue(value);
+	};
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		if (!inputValue) return;
+		if (task.includes(inputValue)) {
+			setInputValue("");
+			return;
+		}
+		setTask((prevTask) => [...prevTask, inputValue]);
+
+		setInputValue("");
+	};
+	return (
+		<>
+			<section className="Todo-container">
+				<h1>To_Do List</h1>
+				<section className="Form-Container">
+					<form onSubmit={handleSubmit}>
+						<div>
+							<input
+								type="text"
+								className="btn-text"
+								autoComplete="off"
+								value={inputValue}
+								onChange={(event) => handleChange(event.target.value)}
+							/>
+						</div>
+						<span>
+							<button type="submit" className="btn-submit">
+								Add Task
+							</button>
+						</span>
+					</form>
+				</section>
+				<section className="unorderedList">
+					<ul>
+						{task.map((CurlElm, index) => {
+							return (
+								<li key={index}>
+									<span>{CurlElm}</span>
+									<button>
+										<MdCheck />
+									</button>
+									<button>
+										<MdDeleteForever />
+									</button>
+								</li>
+							);
+						})}
+					</ul>
+				</section>
+			</section>
+		</>
+	);
+};
